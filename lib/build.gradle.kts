0f078657d4d5f0bfdd094dbf6c5694611512e895
petiosm <bakrfadl1@gmail.com>
2026-03-01 02:40:38 +0300

    Add maven-publish and java-library plugins to build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,19 +1,4 @@
-plugins { 
-    `java-library` 
-}
-
-java { 
-    // Specify the version of Java you want to use
-    toolchain { 
-        languageVersion.set(JavaLanguageVersion.of(11)) 
-    }
-}
-
-repositories { 
-    mavenCentral() 
-}
-
-dependencies { 
-    api("org.jetbrains.kotlin:kotlin-stdlib") 
-    // Add your dependencies here 
+plugins {
+    id("maven-publish")
+    id("java-library")
 }
