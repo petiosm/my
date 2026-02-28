8d0ac90a0338245a51dbddd32b1ee3a97c996584
petiosm <bakrfadl1@gmail.com>
2026-03-01 02:40:53 +0300

    Add maven-publish plugin and publishing configuration for all submodules with Java 11 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,57 +1,30 @@
 plugins {
-    id("java-library")
-    id("maven-publish")
+    kotlin("jvm") version "1.5.30"
+    `maven-publish`
 }
 
 allprojects {
-    group = "com.github.petiosm"
-    version = "1.0.0"
-
-    repositories {
-        mavenCentral()
-    }
+    group = "com.example"
+    version = "0.1.0"
 }
 
 subprojects {
-    apply(plugin = "java-library")
     apply(plugin = "maven-publish")
-
-    java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
 
     publishing {
         publications {
-            register<MavenPublication>("mavenJava") {
+            create<MavenPublication>("maven") {
                 from(components["java"])
-
-                pom {
-                    name.set(project.name)
-                    description.set("Multi-module project with Gradle plugin and runtime")
-                    url.set("https://github.com/petiosm/my")
-
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://opensource.org/licenses/MIT")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("petiosm")
-                            name.set("Petiosm")
-                        }
-                    }
-
-                    scm {
-                        connection.set("scm:git:https://github.com/petiosm/my.git")
-                        developerConnection.set("scm:git:https://github.com/petiosm/my.git")
-                        url.set("https://github.com/petiosm/my")
-                    }
-                }
             }
         }
+        repositories {
+            maven {
+                url = uri("https://my.repository.url")
+            }
+        }
+    }
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
 }
